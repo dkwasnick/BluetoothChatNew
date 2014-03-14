fe9b493043ba5b72cd69e31b5eb08cf8accfbc7a
dkwasnick <daniel@Daniels-MacBook-Pro.local>
2014-03-13 21:00:19 -0700

    removed prompt

--- src/com/example/bluetoothchat/hostChat.java
+++ src/com/example/bluetoothchat/hostChat.java
@@ -116,11 +116,7 @@ public class hostChat extends Activity {
 	            try {
 	                socket = mmServerSocket.accept();
 	                
-	                promptForConnection(socket);
-	                if (!socket.isConnected())
-	                {
-	                	socket = null;
-	                }
+	                
 	                
 	                
 	                
@@ -146,41 +142,18 @@ public class hostChat extends Activity {
 	    }
 	}
 	
-	private void promptForConnection(final BluetoothSocket socket)
-	{
-		DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-		    @Override
-		    public void onClick(DialogInterface dialog, int which) {
-		        switch (which){
-		        case DialogInterface.BUTTON_POSITIVE:
-		            //Yes button clicked
-		            break;
-
-		        case DialogInterface.BUTTON_NEGATIVE:
-		           	try {
-						socket.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-		            break;
-		        }
-		    }
-		    
-		};
-
-		String msg = "Allow "+socket.getRemoteDevice().getName()+" to join chat?";
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setMessage(msg).setPositiveButton("Yes", dialogClickListener)
-		    .setNegativeButton("No", dialogClickListener).show();
-		
-		
-		
-	}
+	
 	
 	private void manageConnectedSocket(BluetoothSocket socket)
 	{
+		
+		
 		System.out.println("BTCHAT: accepted socket! woo");
+		
+
+		
+		
+		
 		sockets.add(socket);
 		
 		final ConnectedThread ct = new ConnectedThread(socket);
